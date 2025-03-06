9d5d19f6981b01c7bea9199185ec2e68318da2eb
Jereme <jeremejazz15@gmail.com>
2025-03-06 16:20:21 +0800

    chore: update android platform

--- android/app/src/main/kotlin/me/jereme/nicedice/nicedice/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package me.jereme.nicedice.nicedice
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'src/app.dart';
 
-
 void main() async {
-
   runApp(NiceDice());
 }
